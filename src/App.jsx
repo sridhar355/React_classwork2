40e4e7e4dd89a6874dfeff32efbf12df0049658a
sridhar <sridhar.meesala2000@gmail.com>
2024-05-29 23:35:39 +0530

    classwork2 created

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='main'>
+      <h1>Learn Web Development</h1>
+      <div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, sunt. Unde ut nemo necessitatibus, sunt ullam minima, molestias provident dignissimos voluptatum, amet incidunt? Quo temporibus aspernatur, earum similique dicta ad?</p>
+      </div>
+      <div>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure maiores doloremque nisi modi eligendi quae corrupti. Beatae, praesentium quo? Ipsa vel asperiores nam sequi iusto illum repellat cupiditate eius et.</p>
+      </div>
+      <div>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed tempore doloribus enim assumenda perspiciatis quisquam ut, corrupti nam, debitis necessitatibus maiores illo autem quos voluptas qui quam quas excepturi porro.</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
